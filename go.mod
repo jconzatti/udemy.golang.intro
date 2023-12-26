6bb94f51c45766d16033a5fc6e9572176a6fceb5
Jhoni Conzatti <jhoni.conzatti@gmail.com>
2023-12-26 17:48:46 -0300

    usando libs do github.com

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module curso
 
 go 1.21.3
+
+require github.com/cod3rcursos/goarea v0.0.0-20171120131752-3ca6d30ad3a4
---
+++ pacote/usandolib/usandolib.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cod3rcursos/goarea"
+)
+
+func main() {
+	fmt.Println(goarea.Circ(5))
+	fmt.Println(goarea.Rect(5, 3))
+}
