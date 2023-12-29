5d3de987d218a4378774c9c0c6bce0b173310eee
Jhoni Conzatti <jhoni.conzatti@gmail.com>
2023-12-29 19:02:21 -0300

    testes

---
+++ testes/matematica/matematica.go
@@ -0,0 +1,17 @@
+package matematica
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// Media calcula a média de uma lista de números reais
+func Media(numeros ...float64) float64 {
+	total := 0.0
+	for _, numero := range numeros {
+		total += numero
+	}
+	media := total / float64(len(numeros))
+	mediaArredondada, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", media), 64)
+	return mediaArredondada
+}
